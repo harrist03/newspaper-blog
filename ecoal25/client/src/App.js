64c4c37fb5cb56003b1963a5fde30931ebae4169
Alezandrio <alexandremartaa@gmail.com>
2025-03-04 15:56:25 +0000

    terça-feira - NewArticle

--- ecoal25/client/src/App.js
+++ ecoal25/client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import {Route, Routes, Link} from "react-router-dom";
 import './App.css';
 import Home from "./Home/Home";
+import NewArticle from './components/Addarticle/NewArticle';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         </div>
         <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/newarticle" element={<NewArticle/>}/>
         </Routes>
       </>
   );
