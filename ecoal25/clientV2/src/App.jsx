ce91b7584e4be061cca0314ae1f5a277b3601278
Loup Olivier <loup.olivier@gmail.com>
2025-03-05 10:24:54 +0100

    New archive of the project ClientV2

---
+++ ecoal25/clientV2/src/App.jsx
@@ -0,0 +1,27 @@
+import {Route, Routes, Link} from "react-router-dom";
+import './App.css';
+import Home from "./components/Home/Home";
+import NewArticle from './components/Addarticle/NewArticle';
+import Login from "./components/login_page/Login";
+import NavBar from "./components/NavBar/NavBar";
+import Articles from "./components/Articles/Articles";
+import Article from "./components/Article/Article";
+
+function App() {
+    return (
+        <>
+            <NavBar/>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/article" element={<Articles/>}/>
+                    <Route path="/article/:id" element={<Article/>}/>
+                    <Route path="/newarticle" element={<NewArticle/>}/>
+                </Routes>
+            </div>
+        </>
+    );
+}
+
+export default App;
