23c2a1877cd094903f698c4c2ed8676506ab5b98
Poulain <poul1matheo@mail.com>
2025-03-06 16:36:13 +0100

    change navbar order

--- ecoal25/clientV2/src/components/NavBar/NavBar.jsx
+++ ecoal25/clientV2/src/components/NavBar/NavBar.jsx
@@ -35,6 +35,10 @@ function NavBar() {
                             <Link onClick={handleClick} to="/">Home</Link>
                         </li>
 
+                        <li>
+                            <Link onClick={handleClick} to="/article">Articles</Link>
+                        </li>
+
                         {IsLoggedIn ? (
                             <>
                                 <li>
@@ -46,9 +50,7 @@ function NavBar() {
                                 <Link onClick={handleClick} to="/login">Login / Register</Link>
                             </li>
                         )}
-                        <li>
-                            <Link onClick={handleClick} to="/article">Articles</Link>
-                        </li>
+                        
                         {IsLoggedIn && (
                             <li>
                                 <Link onClick={handleClick} to="/newarticle">Create Article</Link>
