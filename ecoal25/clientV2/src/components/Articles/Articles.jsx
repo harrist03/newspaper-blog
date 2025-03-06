b42af0ad26de72bd4646f11296fe13a47c5ca340
Loup Olivier <loup.olivier@gmail.com>
2025-03-06 10:01:18 +0100

    Form modification to add media type

--- ecoal25/clientV2/src/components/Articles/Articles.jsx
+++ ecoal25/clientV2/src/components/Articles/Articles.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Articles.css";
-import { useNavigate } from "react-router-dom";
 import Search from "../Search/Search";
 import './Articles.css';
 import {useNavigate} from 'react-router-dom';
